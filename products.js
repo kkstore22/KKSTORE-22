3922319e29a818b033d61c901662c29ca41d1ad4
kkstore22 <kishorkaranjekar.ak@gmail.com>
2026-07-22 23:30:24 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,97 +1,117 @@
 const products = [
 
 {
-id:1,
-name:"Women's Kurti",
-Price:"₹299",
-image:"https://m.media-amazon.com/images/I/61q9J81mU5L._SY741_.jpg",
-Buy Now:"https://link.amazon/B0iQoOycL"
+    id:1,
+    name:"Women's Kurti",
+    image:"https://m.media-amazon.com/images/I/61q9J81mU5L._SY741_.jpg",
+    rating:"4.5",
+    delivery:"Free Delivery",
+    category:"Womens Fashion",
+    link:"https://link.amazon/B0iQoOycL"
 },
 
 {
-id:2,
-name:"Saree",
-undar:"₹499",
-image:"https://m.media-amazon.com/images/I/71zD6LNDRdL._SX679_.jpg",
-affiliate:"https://link.amazon/B09Kumugl"
+    id:2,
+    name:"Women's Saree",
+    image:"https://m.media-amazon.com/images/I/71zD6LNDRdL._SX679_.jpg",
+    rating:"4.4",
+    delivery:"Free Delivery",
+    category:"Womens Fashion",
+    link:"https://link.amazon/B09Kumugl"
 },
 
 {
-id:3,
-name:"westan wear",
-undar:"₹999",
-image:"https://m.media-amazon.com/images/I/713jWuxPG6L._SX679_.jpg",
-affiliate:"https://link.amazon/A09knf8B4"
-},
-
-{
-id:4,
-name:"Ethnic Wear",
-undar:"₹999",
-image:"https://m.media-amazon.com/images/I/71fv2bcy+4L._SY879_.jpg",
-affiliate:"https://link.amazon/B06NNROjZ"
-},
-
-{
-id:5,
-name:"Men t-sharts",
-undar:"₹199",
-image:"https://m.media-amazon.com/images/I/61LjId389cL._AC_UL480_QL65_.jpg",
-affiliate:"https://link.amazon/B08Ka8OQt"
-},
-
-{
-id:6,
-name:"Combo Offers undar ",
-undar:"₹500",
-image:"https://m.media-amazon.com/images/I/61Z-qQuz3XL._AC_UL480_FMwebp_QL65_.jpg",
-affiliate:"https://link.amazon/B0bMMguFr"
-},
-{
-id:7,
-name:"Mens shart",
-undar:"₹499",
-image:"https://m.media-amazon.com/images/I/61GOTzXwn8L._AC_UL480_FMwebp_QL65_.jpg",
-affiliate:"https://link.amazon/B0iUrkCkD"
-},
-
-{
-id:8,
-name:"Wireless Earbuds",
-undar:"₹999",
-image:"https://m.media-amazon.com/images/I/61ddMjOHZXL._AC_UL480_FMwebp_QL65_.jpg",
-affiliate:"https://link.amazon/B0935GPxA"
-},
-
-{
-id:9,
-name:"Smart Watch",
-undar:"₹999",
-image:"https://m.media-amazon.com/images/I/71dd68N70FL._AC_UY327_FMwebp_QL65_.jpg",
-affiliate:"https://link.amazon/B06PixhCj"
-},
-{
-id:10,
-name:"Mobile covers",
-undar:"₹299",
-image:"https://m.media-amazon.com/images/I/41Cqs5O+2CL._SY300_SX300_QL70_FMwebp_.jpg",
-affiliate:"https://link.amazon/B09RrxkLC"
-},
-
-{
-id:11,
-name:"Power Bank",
-undar:"₹999",
-image:"https://m.media-amazon.com/images/I/61FEiRJcFiL._AC_UL480_FMwebp_QL65_.jpg",
-affiliate:"https://link.amazon/B050lJRDh"
-},
-
-{
-id:12,
-name:"Kitchen Gadgets",
-price:"₹999",
-image:"https://m.media-amazon.com/images/I/71Yna+hE0wL._AC_UL480_FMwebp_QL65_.jpg",
-affiliate:"https://link.amazon/B08SqblYW"
+    id:3,
+    name:"westan wear",
+    image:"https://m.media-amazon.com/images/I/713jWuxPG6L._SX679_.jpg",
+    rating:"4.3",
+    delivery:"Free Delivery",
+    category:"Womens Fashion",
+    link:"https://link.amazon/A09knf8B4"
 }
-];
+{
+    id:4,
+    name:"Ethnic Wear",
+    image:"https://m.media-amazon.com/images/I/71fv2bcy+4L._SY879_.jpg",
+    rating:"4.5",
+    delivery:"Free Delivery",
+    category:"Womans Fashion",
+    link:"https://link.amazon/B06NNROjZ"
+},
 
+{
+    id:5,
+    name:"Men t-sharts",
+    image:"https://m.media-amazon.com/images/I/61LjId389cL._AC_UL480_QL65_.jpg",
+    rating:"4.4",
+    delivery:"Free Delivery",
+    category:"Mens Fashion",
+    link:"https://link.amazon/B08Ka8OQt"
+},{
+    id:6,
+    name:"Combo Offers undar ",
+    image:"https://m.media-amazon.com/images/I/61Z-qQuz3XL._AC_UL480_FMwebp_QL65_.jpg",
+    rating:"4.5",
+    delivery:"Free Delivery",
+    category:"Home & Kitchan",
+    link:"https://link.amazon/B0bMMguFr"
+},
+
+{
+    id:7,
+    name:"Mens shart",
+    image:"https://m.media-amazon.com/images/I/61GOTzXwn8L._AC_UL480_FMwebp_QL65_.jpg",
+    rating:"4.4",
+    delivery:"Free Delivery",
+    category:"Mens Fashion",
+    link:"https://link.amazon/B0iUrkCkD"
+},
+
+{
+    id:8,
+    name:"Wireless Earbuds",
+    image:"https://m.media-amazon.com/images/I/61ddMjOHZXL._AC_UL480_FMwebp_QL65_.jpg",
+    rating:"4.5",
+    delivery:"Free Delivery",
+    category:"Mobile & Accessories",
+    link:"https://link.amazon/B0935GPxA"
+},
+
+{
+    id:9,
+    name:"Smart Watch",
+    image:"https://m.media-amazon.com/images/I/71dd68N70FL._AC_UY327_FMwebp_QL65_.jpg",
+    rating:"4.4",
+    delivery:"Free Delivery",
+    category:"Mobile & Accessories",
+    link:"https://link.amazon/B06PixhCj"
+},{
+    id:10,
+    name:"Mobile covers",
+    image:"https://m.media-amazon.com/images/I/41Cqs5O+2CL._SY300_SX300_QL70_FMwebp_.jpg",
+    rating:"4.5",
+    delivery:"Free Delivery",
+    category:"Mobile & Accessories",
+    link:"https://link.amazon/B09RrxkLC"
+},
+
+{
+    id:11,
+    name:"Power Bank",
+    image:"images/saree1.jpg",
+    rating:"4.4",
+    delivery:"Free Delivery",
+    category:"Mobile & Accessories",
+    link:"https://amzn.to/yyyyyyyy"
+},
+{
+    id:12,
+    name:"Kitchen Gadgets",
+    image:"https://m.media-amazon.com/images/I/71Yna+hE0wL._AC_UL480_FMwebp_QL65_.jpg",
+    rating:"4.5",
+    delivery:"Free Delivery",
+    category:"Home & Kitchan",
+    link:"https://link.amazon/B08SqblYW"
+},
+
+];
