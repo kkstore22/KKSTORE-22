59fcaa30e814597362ee6d9fc7f0e36085748e8a
kkstore22 <kishorkaranjekar.ak@gmail.com>
2026-07-24 23:20:59 +0530

    Refactor products array to include duplicates

--- products.js
+++ products.js
@@ -11,4 +11,388 @@ const products = [
     image:"https://picsum.photos/300/350",
     link:"#"
 }
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{    
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{    
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{    
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{    
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{    
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{    
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{    
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{    
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{    
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{    
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
+{    
+    id:1,
+    name:"Test Product",
+    category:"Women",
+    price:299,
+    oldPrice:999,
+    discount:"70% OFF",
+    rating:4.5,
+    delivery:"Free Delivery",
+    image:"https://picsum.photos/300/350",
+    link:"#"
+}
 ];
