08f4f656e007ad709731d6b6545cd708b865afe8
kkstore22 <kishorkaranjekar.ak@gmail.com>
2026-07-24 22:48:47 +0530

    Revise product list with new details and links
    Updated product details including prices and images.

--- products.js
+++ products.js
@@ -1,42 +1,42 @@
 const products = [
 
 {
-    id:1,
-    name:"Women's Floral Kurti",
-    category:"Women",
-    price:299,
-    oldPrice:999,
-    rating:4.4,
-    discount:"70% OFF",
-    delivery:"Free Delivery",
-    image:"https://picsum.photos/300/350?random=1",
-    link:"#"
+    id: 1,
+  name: "Women's Floral Kurti",
+  category: "Women",
+  price: 220,
+  oldPrice: 1999,
+  discount: "70% OFF",
+  rating: 4.3,
+  delivery: "Free Delivery",
+  image: "https://rukminim2.flixcart.com/image/612/612/xif0q/shopsy-kurta/1/7/i/-resized-original-imahhx3ctnqfafhn.jpeg?q=70",
+  link: "https://fktr.in/hjfa880"
 },
 
 {
-    id:2,
-    name:"Cotton Saree",
-    category:"Women",
-    price:499,
-    oldPrice:1499,
-    rating:4.5,
-    discount:"67% OFF",
-    delivery:"Free Delivery",
-    image:"https://picsum.photos/300/350?random=2",
-    link:"#"
+    id: 2,
+  name: "Women's Floral Kurti",
+  category: "Women",
+  price: 388,
+  oldPrice: 999,
+  discount: "70% OFF",
+  rating: 4.3,
+  delivery: "Free Delivery",
+  image: "https://rukminim1.flixcart.com/image/3072/3072/xif0q/kurta/o/n/i/l-vc-red-sambshiv-original-imahpcujf33yapr2.jpeg?q=90",
+  link: "https://fktr.in/z7tk6Q3"
 },
 
 {
-    id:3,
-    name:"Men's Casual Shirt",
-    category:"Men",
-    price:399,
-    oldPrice:999,
-    rating:4.3,
-    discount:"60% OFF",
-    delivery:"Free Delivery",
-    image:"https://picsum.photos/300/350?random=3",
-    link:"#"
+      id: 3,
+  name: "Kurtis & Kurta Sets",
+  category: "Women",
+  price: 299
+  oldPrice: 999,
+  discount: "70% OFF",
+  rating: 4.3,
+  delivery: "Free Delivery",
+  image: "https://rukminim2.flixcart.com/image/612/612/xif0q/ethnic-set/z/r/v/xxl-tie-dye-001-fabunna-cotton-resized-original-imahgbgawqeadk8q.jpeg?q=70",
+  link: "https://fktr.in/46fMzrq"
 },
 
 {
