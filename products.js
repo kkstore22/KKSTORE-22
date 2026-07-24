eca66255d501519b59f029ade86f245bf599848b
kkstore22 <kishorkaranjekar.ak@gmail.com>
2026-07-25 00:06:53 +0530

    Refactor product data with updated information
    Updated product details including images and links.

--- products.js
+++ products.js
@@ -23,21 +23,21 @@ oldPrice:999,
 discount:"50% OFF",
 rating:4.4,
 delivery:"Free Delivery",
-image:"https://picsum.photos/300/350?2",
-link:"#"
+image:"https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/x/v/d/s-st2-vebnor-original-imahhy7edatnmfvx.jpeg?q=70",
+link:"https://fktr.in/z5Pd9Uh"
 },
 
 {
 id:3,
-name:"Samsung Mobile",
+name:"Mobile undar 25000",
 category:"Mobile",
 price:14999,
 oldPrice:18999,
 discount:"20% OFF",
 rating:4.6,
 delivery:"Free Delivery",
-image:"https://picsum.photos/300/350?3",
-link:"#"
+image:"https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/y/i/n/-original-imahpgktvxzebfkq.jpeg?q=70",
+link:"https://fktr.in/rsegQ1i"
 },
 
 {
@@ -49,8 +49,8 @@ oldPrice:1999,
 discount:"55% OFF",
 rating:4.4,
 delivery:"Free Delivery",
-image:"https://picsum.photos/300/350?4",
-link:"#"
+image:"https://www.flipkart.com/quicky-tws-bluetooth-earbuds-clear-calls-portable-charging-case/p/itm2c1204807a1da?pid=ACCHPBRDK8TYBAGF&lid=LSTACCHPBRDK8TYBAGFXHDI2J&marketplace=FLIPKART&q=Bluetooth+Earbuds&store=0pm%2Ffcn%2F821&srno=s_1_14&otracker=search&otracker1=search&fm=Search&iid=bca15cb7-87e4-4874-89be-1094fafa0d16.ACCHPBRDK8TYBAGF.SEARCH&ppt=sp&ppn=sp&qH=340c82ff6c8b739b&ov_redirect=true&ov_redirect=true",
+link:"https://fktr.in/8TUklpI"
 },
 
 {
@@ -62,8 +62,8 @@ oldPrice:399,
 discount:"50% OFF",
 rating:4.2,
 delivery:"Free Delivery",
-image:"https://picsum.photos/300/350?5",
-link:"#"
+image:"https://rukminim2.flixcart.com/image/612/612/xif0q/shopsy-face-wash/a/s/a/-original-imahzd78b7gdsmrd.jpeg?q=70",
+link:"https://fktr.in/Yk5ylyu"
 },
 
 {
@@ -75,8 +75,8 @@ oldPrice:1599,
 discount:"50% OFF",
 rating:4.3,
 delivery:"Free Delivery",
-image:"https://picsum.photos/300/350?6",
-link:"#"
+image:"https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/d/a/p/-watermarked-original-imahfhpnzwfv9fxq.jpeg?q=70",
+link:"https://fktr.in/GB04VXN"
 },
 
 {
@@ -88,8 +88,8 @@ oldPrice:2499,
 discount:"40% OFF",
 rating:4.5,
 delivery:"Free Delivery",
-image:"https://picsum.photos/300/350?7",
-link:"#"
+image:"https://rukminim2.flixcart.com/image/612/612/xif0q/mixer-grinder-juicer/s/k/x/powerful-740-watt-havok-original-imah8bfmb8gwyjy5.jpeg?q=70",
+link:"https://www.flipkart.com/search?q=Kitchen%20Mixer&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"
 },
 
 {
@@ -101,8 +101,8 @@ oldPrice:799,
 discount:"25% OFF",
 rating:4.4,
 delivery:"Free Delivery",
-image:"https://picsum.photos/300/350?8",
-link:"#"
+image:"https://rukminim2.flixcart.com/image/612/612/xif0q/rice/y/b/p/10-daily-dubar-finest-quality-white-na-pouch-basmati-rice-original-imahk5zvedqkvrkn.jpeg?q=70",
+link:"https://fktr.in/wBr19x3"
 },
 
 {
@@ -114,8 +114,8 @@ oldPrice:2499,
 discount:"60% OFF",
 rating:4.5,
 delivery:"Free Delivery",
-image:"https://picsum.photos/300/350?9",
-link:"#"
+image:"https://rukminim2.flixcart.com/image/612/612/xif0q/smartwatch/n/o/z/-enriched-transparent-original-imah76jstup5zdww.png?q=70",
+link:"https://fktr.in/0itOSjN"
 }
 
 ];
